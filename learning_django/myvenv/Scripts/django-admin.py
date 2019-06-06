153948218138e93a7f158b4faac9d51e923a5b53
pietercusters <pietercusters@vandebron.nl>
2019-06-06 10:54:58 +0200

    learning_django

---
+++ learning_django/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pieter~1\docume~1\github\script~1\python\bi\learni~1\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
